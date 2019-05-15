73d9bfaa093ab05a9be1d130433e649240902473
Robin Douglas <robin.1996@live.co.uk>
2019-05-15 17:07:53 +0100

    Initial commit

---
+++ OMDb-client/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OMDb-client
+//
+//  Created by Robin Douglas on 15/05/2019.
+//  Copyright © 2019 Robin Douglas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
